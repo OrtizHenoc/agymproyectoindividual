4e7bc46864b9806aaf95d40058d65992b0a39427
jhoz <henoczamora22@gmail.com>
2023-05-21 16:34:36 -0500

    Responsive de boton de cuenta, menu hamburguesa y header y footer faltan detalles

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(main);
 
 var contador = true;
-
+var desplegado = true;
 function main(){
     $('.bt-menu').click(function(){
         toggleNav();
@@ -12,8 +12,37 @@ function main(){
             closeNav();
         }
     });
+
+    $('.cuenta-container').click(function(){
+        toggleUser();
+    });
+
+    $(document).click(function(event){
+        if(!$(event.target).closest('.cuenta-list').length && !$(event.target).closest('.cuenta-container').length) {
+            cerrarDesplegable();
+        }
+    });
+
+}
+function toggleUser(){
+    if(desplegado == true){
+        $('.cuenta-list').animate({
+            left:'0',
+            top:'3%'
+        });
+        desplegado = false;
+    } else {
+        cerrarDesplegable();
+    }
 }
 
+function cerrarDesplegable(){
+    $('.cuenta-list').animate({
+        left: '-100%',
+        top:'3%'
+    });
+    desplegado = true;
+}
 function toggleNav() {
     if(contador == true){
         $('nav').animate({
