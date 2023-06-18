797580443eed023c4315c3e7e71c2142258118db
jhoz <henoczamora22@gmail.com>
2023-06-18 02:09:20 -0500

    Se agregaron graficos en la seccion de admin, y las estadisticas, sin responsive

---
+++ js/grafico.js
@@ -0,0 +1,78 @@
+const bars = document.getElementById('grafico-barras');
+const pie = document.getElementById('grafico-pastel');
+var layout1 = {
+    paper_bgcolor:'#1f1e1e',
+    plot_bgcolor:'#1f1e1e',
+    xaxis: {
+        tickfont: {
+          color: '#ffffff'
+        }
+      },
+      yaxis: {
+        tickfont: {
+          color: '#ffffff'
+        }
+      },
+    title:{
+        text:'Ventas de los ultimos 6 meses',
+        font:{
+            color:'#ffffff'
+        }
+    },
+    autosize: true,
+}
+var data1 = [{
+    x: ['Febrero','Marzo','Abril','Mayo','Junio','Julio'],
+    y: [12000,10150,13450,16000,9000,11200,15000],
+    type:'bar',
+    marker:{
+        color:[
+            '#003f5c',
+            '#444e86',
+            '#955196',
+            '#dd5182',
+            '#ff6e54',
+            '#ffa600',
+        ]
+      },
+    }
+  
+];
+var layout2 = {
+    paper_bgcolor:'#1f1e1e',
+    plot_bgcolor:'#1f1e1e',
+    title:{
+        text:'Tipo más vendido',
+        font:{
+            color:'#ffffff'
+        }
+    },
+    legend: {
+        font: {
+          color: '#ffffff' // Color de texto de la leyenda
+        }
+    },
+    margin: {"t": 0, "b": 0, "l": 0, "r": 0},
+    autosize: true,
+}
+var data2 = [{
+    labels: ['Coche Estándar','Furgoneta','Coche Industrial','Accesorio'],
+    values: [100,40,20,250],
+    type:'pie',
+    marker:{
+        colors:[
+            '#003f5c',
+            '#7a5195',
+            '#ffa600',
+            '#ef5675',
+        ]
+    },
+    textfont: {
+        color: '#ffffff'
+    },
+    position: 'middle-center',
+    automargin: true,
+}];
+Plotly.newPlot(bars,data1,layout1);
+Plotly.newPlot(pie,data2,layout2);
+
