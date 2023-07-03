a9f6816cf356a20b93c065b9f7cd477b79d75675
jhoz <henoczamora22@gmail.com>
2023-07-02 21:50:28 -0500

    Probando camara

--- js/tomarfoto.js
+++ js/tomarfoto.js
@@ -8,7 +8,23 @@ Webcam.attach("#camera")
 
 function takess(){
     Webcam.snap(function(data_uri){
-        document.getElementById("circle-container").innerHTML = 
-        `<img src="${data_uri}">`;
+        var cameraDiv = document.getElementById("camera");
+        if(cameraDiv.style.display === "none"){
+            alert("Desplega la camara para tomar la foto")
+        }else{
+            document.getElementById("circle-container").innerHTML = 
+            `<img src="${data_uri}">`;
+        }
     });
 }
+
+function toggleCameraDisplay() {
+    var cameraDiv = document.getElementById("camera");
+    var computedStyle = window.getComputedStyle(cameraDiv);
+    var displayValue = computedStyle.getPropertyValue("display");
+    if (displayValue === "none") {
+      cameraDiv.style.display = "block";
+    } else {
+      cameraDiv.style.display = "none";
+    }
+  }
